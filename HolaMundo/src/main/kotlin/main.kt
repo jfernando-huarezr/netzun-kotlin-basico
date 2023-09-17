c0a5f29db27c74687a6284bb6318e713bbb58f3f
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-17 11:32:46 -0500

    elvis operator

--- HolaMundo/src/main/kotlin/main.kt
+++ HolaMundo/src/main/kotlin/main.kt
@@ -15,7 +15,7 @@ fun main() {
     //var saludos = "Hola Mundo"
 
 
-    //STRINGS
+    //*****STRINGS
 
 //    var nombre = "Peter"
 //    var apellido ="Parker"
@@ -25,7 +25,7 @@ fun main() {
 //    println("$nombre $apellido")
 
 
-    //NUMEROS
+    //*****NUMEROS
 
 //    var numero1 = 4
 //    var numero2 = 6
@@ -34,7 +34,7 @@ fun main() {
 //    println("Multiplicacion ${numero1*numero2}")
 //    println("Los numeros son ${numero1} y ${numero2}")
 
-    //NULLABLES
+    //*****NULLABLES
         //no puedo hacer esto
 //    var nombre : String
 //    nombre = null
@@ -46,14 +46,24 @@ fun main() {
 
 
     //null safe
-    var name : String?
-    name = "Peter"
-    var longitud : Int? = name.length
-    println(longitud)
-    name = null
-    longitud = name?.length
-    println(longitud)
+//    var name : String?
+//    name = "Peter"
+//    var longitud : Int? = name.length
+//    println(longitud)
+//    name = null
+//    longitud = name?.length
+//    println(longitud)
 
     //si coloco name!! retiro esta caracteristica de Java
+
+    //****ELVIS OPERATOR ?:
+    //Cuando llegue un parametro null genera errores como se vio en la parte anterior
+    //valida que una variable no sea nula, y si es nula asigna otro valor
+
+    var nombre: String?
+    nombre = null ?: "Usuario"
+
+    var longitud: Int? = nombre?.length ?: 0
+    println("Hola ${nombre} tu nombre tiene ${longitud} letras")
 
 }
