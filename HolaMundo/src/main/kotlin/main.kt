65d0949327d704c02f8f81a384034784676cdb5c
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-17 10:58:01 -0500

    curso kotlin basico

---
+++ HolaMundo/src/main/kotlin/main.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    // var-val NombreVariable : TipoVariable = Valor
+    //variable
+    //var numeroVar : Int = 4
+
+    //constante
+    //val numeroVal : Int = 25
+
+    //println(numeroVar)
+    //println(numeroVal)
+
+    //Inferencia de tipos. Kotlin identifica automaticamente el tipo de dato que usamos
+    //Sin embargo, si la variable empezo como String, luego ya no se puede cambiar y se queda asi
+    //var saludos = "Hola Mundo"
+
+
+    //STRINGS
+
+    var nombre = "Peter"
+    var apellido ="Parker"
+
+    println(nombre + " " + apellido)
+    //String Interpolation
+    println("$nombre $apellido")
+
+}
